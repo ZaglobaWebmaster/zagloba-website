9059a12dbc916d588ec9cffa74b1f0affc0727e8
ZaglobaWebmaster <zaglobawebmaster@gmail.com>
2018-09-25 21:20:00 -0500

    "Update"

--- update-website.py
+++ update-website.py
@@ -7,26 +7,33 @@ call(["git", "push"])
 
 doc = Document("frontpage.docx")
 p = doc.paragraphs
-for i in range(len(p)):
+r = range(len(p))
+for i in r:
 	text = p[i].text
+	r.remove(i)
 	if text == "BANNER":
 		i += 1
+		r.remove(i)
 		print("Banner Text:")
 		print(p[i].text)
 	elif text == "EVENT":
 		i += 1
+		r.remove(i)
 		print("Event Details:")
 		event_name = str(p[i].text).replace("Name: ", "")
 		i += 1
+		r.remove(i)
 		event_name += "\n"
 		event_details = str(p[i].text).replace("Location: ", "")
 		i += 1
+		r.remove(i)
 		event_details += " - "
 		event_details += str(p[i].text).replace("Date: ", "")
 		i += 1
+		r.remove(i)
 		event_details += " - "
 		event_details += str(p[i].text).replace("Time: ", "")
-		print(event_name + event_name)
+		print(event_name + event_details)
 	print(i)
 
 input()
